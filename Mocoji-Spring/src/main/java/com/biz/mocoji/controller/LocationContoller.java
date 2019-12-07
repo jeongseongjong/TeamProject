f7a2b06381c3fd76deeae3bb274f9e3bb56b0f10
jeongseongjong <jjong9950@naver.com>
2019-12-07 14:59:18 +0900

    리스트수정

--- Mocoji-Spring/src/main/java/com/biz/mocoji/controller/LocationContoller.java
+++ Mocoji-Spring/src/main/java/com/biz/mocoji/controller/LocationContoller.java
@@ -3,7 +3,6 @@ package com.biz.mocoji.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
